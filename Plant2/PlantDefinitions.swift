1dae985048f1ce24344bc157481890d83bc3d413
hashbrun554 <hashbrun444@gmail.com>
2023-11-10 21:33:34 -1000

    add attributes and identifiers

--- Plant2/PlantDefinitions.swift
+++ Plant2/PlantDefinitions.swift
@@ -9,75 +9,75 @@ import SwiftUI
 
 // Begin Plant Attributes
 
-let Plant1Attributes = [1, 1, 1, 1, 1]
-let Plant2Attributes = [1, 1, 1, 1, 1]
-let Plant3Attributes = [1, 1, 1, 1, 1]
-let Plant4Attributes = [1, 1, 1, 1, 1]
-let Plant5Attributes = [1, 1, 1, 1, 1]
-let Plant6Attributes = [1, 1, 1, 1, 1]
-let Plant7Attributes = [1, 1, 1, 1, 1]
-let Plant8Attributes = [1, 1, 1, 1, 1]
-let Plant9Attributes = [1, 1, 1, 1, 1]
+let Plant1Attributes = [1, 1, 3, 1, 1]
+let Plant2Attributes = [1, 1, 1, 3, 5]
+let Plant3Attributes = [2, 3, 2, 2, 1]
+let Plant4Attributes = [2, 1, 2, 1, 1]
+let Plant5Attributes = [2, 1, 2, 1, 4]
+let Plant6Attributes = [1, 3, 2, 2, 5]
+let Plant7Attributes = [1, 3, 1, 2, 2] // Demo plant
+let Plant8Attributes = [1, 3, 1, 2, 2] // Demo plant
+let Plant9Attributes = [2, 3, 1, 3, 3]
 
 // End Plant Attributs
 
-// Bunny ear cactus
+// Bunny ear cactus (1)
 struct Plant1Preview: View {
     var body: some View {
         Text("Bunny ear cactus")
     }
 }
 
-// Orchid
+// Orchid (2)
 struct Plant2Preview: View {
     var body: some View {
         Text("Orchid")
     }
 }
 
-// Areca Palm Plant
+// Areca Palm Plant (3)
 struct Plant3Preview: View {
     var body: some View {
         Text("Areca Palm Plant")
     }
 }
 
-// Snake plant
+// Snake plant (4)
 struct Plant4Preview: View {
     var body: some View {
         Text("Snake plant")
     }
 }
 
-// Aloe Vera
+// Aloe Vera (5)
 struct Plant5Preview: View {
     var body: some View {
         Text("Aloe Vera")
     }
 }
 
-// Laceleaf
+// Laceleaf (6)
 struct Plant6Preview: View {
     var body: some View {
         Text("Laceleaf")
     }
 }
 
-// Basil
+// Basil (7)
 struct Plant7Preview: View {
     var body: some View {
         Text("Basil")
     }
 }
 
-// Rosemary
+// Rosemary (8)
 struct Plant8Preview: View {
     var body: some View {
         Text("Rosemary")
     }
 }
 
-// Cherry Tomatoes
+// Cherry Tomatoes (9)
 struct Plant9Preview: View {
     var body: some View {
         Text("Cherry Tomatoes")
