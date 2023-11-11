83d13e10b43fbe3560f99bd80e4fa5655dcfb895
hashbrun554 <hashbrun444@gmail.com>
2023-11-10 21:16:01 -1000

    add some attributes

---
+++ Plant2/PlantDefinitions.swift
@@ -0,0 +1,86 @@
+//
+//  PlantsPreviews.swift
+//  Growacle
+//
+//  Created by Cristian Matache on 11/10/23.
+//
+
+import SwiftUI
+
+// Begin Plant Attributes
+
+let Plant1Attributes = [1, 1, 1, 1, 1]
+let Plant2Attributes = [1, 1, 1, 1, 1]
+let Plant3Attributes = [1, 1, 1, 1, 1]
+let Plant4Attributes = [1, 1, 1, 1, 1]
+let Plant5Attributes = [1, 1, 1, 1, 1]
+let Plant6Attributes = [1, 1, 1, 1, 1]
+let Plant7Attributes = [1, 1, 1, 1, 1]
+let Plant8Attributes = [1, 1, 1, 1, 1]
+let Plant9Attributes = [1, 1, 1, 1, 1]
+
+// End Plant Attributs
+
+// Bunny ear cactus
+struct Plant1Preview: View {
+    var body: some View {
+        Text("Bunny ear cactus")
+    }
+}
+
+// Orchid
+struct Plant2Preview: View {
+    var body: some View {
+        Text("Orchid")
+    }
+}
+
+// Areca Palm Plant
+struct Plant3Preview: View {
+    var body: some View {
+        Text("Areca Palm Plant")
+    }
+}
+
+// Snake plant
+struct Plant4Preview: View {
+    var body: some View {
+        Text("Snake plant")
+    }
+}
+
+// Aloe Vera
+struct Plant5Preview: View {
+    var body: some View {
+        Text("Aloe Vera")
+    }
+}
+
+// Laceleaf
+struct Plant6Preview: View {
+    var body: some View {
+        Text("Laceleaf")
+    }
+}
+
+// Basil
+struct Plant7Preview: View {
+    var body: some View {
+        Text("Basil")
+    }
+}
+
+// Rosemary
+struct Plant8Preview: View {
+    var body: some View {
+        Text("Rosemary")
+    }
+}
+
+// Cherry Tomatoes
+struct Plant9Preview: View {
+    var body: some View {
+        Text("Cherry Tomatoes")
+    }
+}
+
